e2f285e4bf4f4cda417ac531963bf6af689204ea
sameerchauhan1 <sameerchauhan11o8@gmail.com>
2024-04-08 16:18:53 +0530

    first-commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/Appbar";
+
+export default function Page(): JSX.Element {
+   const session = useSession();
+   return (
+      <div>
+         <Appbar
+            onSignin={signIn}
+            onSignout={signOut}
+            user={session.data?.user}
+         />
+      </div>
+   );
+}
